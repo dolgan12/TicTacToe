470575d6a6bf4a940ccdfec2eb24f470785660f4
Conny Garpö <dolgan12@gmail.com>
2016-01-27 16:22:57 +0100

    Updated game logic.
    Redid the game logic with a TicTacToe object and added the function to its prototype

--- js/app.js
+++ js/app.js
@@ -9,12 +9,18 @@ MiniMax:
 3 Algorith then starts from botom of tree, deciding best move for that player and removing all bad moves.
 */
 
+/* Create a game object */
+var TicTacToe = function(){
+  this.player = 'o';
+  this.computer = 'x';
+};
+
 
 /* Win state
   all winning combinations in if-statement
 */
 
-function checkWin(player, board){
+TicTacToe.prototype.checkWin = function(player, board){
   if(
       (board[0] == player && board[1] == player && board[2] == player) ||
       (board[3] == player && board[4] == player && board[5] == player) ||
@@ -29,23 +35,98 @@ function checkWin(player, board){
  }else{
    return false;
  }
-}
+};
 
 /* check for a tie
   loop the board and if a square is empty return false;
 */
-function checkTie(board){
+TicTacToe.prototype.checkTie = function(board){
   for(var i = 0; i < bord.length; i ++){
     if(board[i] === 0){
       return false;
     }
   }
   return true;
-}
+};
 
 /* Place in random corner or center
   random even  up to 8.
 */
-function firstMove(){
+TicTacToe.prototype.firstMove = function(){
   return Math.floor(Math.random() * (5)) * 2;
-}
+};
+TicTacToe.prototype.copyBoard = function(board){
+  return board.slice(0);
+};
+
+TicTacToe.prototype.checkMove = function(player, move, board){
+  var newBoard = this.copyBoard(board);
+  if(newBoard[move] === 0){
+    newBoard[move] = player;
+    return newBoard;
+  }else {
+    return null;
+  }
+};
+
+TicTacToe.prototype.findMove = function(board){
+  var bestMoveValue = -100;
+  var move = 0;
+  for(var i = 0; i < bord.length; i++){
+    var newBoard = this.checkMove(i, this.computer, board);
+    if(newBoard){
+      var predictedMoveValue = this.minValue(newBoard);
+      if(predictedMoveValue > bestMoveValue) {
+        bestMoveValue = predictedMoveValue;
+        move = i;
+      }
+    }
+  }
+  return move;
+};
+TicTacToe.prototype.minValue = function(board){
+  if(this.checkWin(this.player, board)) {
+    return -1;
+  }else if (this.checkTie(board)) {
+    return 0;
+  }else if(this.checkWin(this.computer, board)){
+    return 1;
+  }else {
+    var bestMoveValue = 100;
+    var move = 0;
+    for(var i = 0; i < bord.length; i++){
+      var newBoard = this.checkMove(i, this.player, board);
+      if(newBoard){
+        var predictedMoveValue = this.maxValue(newBoard);
+        if(predictedMoveValue < bestMoveValue) {
+          bestMoveValue = predictedMoveValue;
+          move = i;
+        }
+      }
+    }
+    return bestMoveValue;
+  }
+};
+TicTacToe.prototype.maxValue = function(board){
+  if(this.checkWin(this.player, board)) {
+    return -1;
+  }else if (this.checkTie(board)) {
+    return 0;
+  }else if(this.checkWin(this.computer, board)){
+    return 1;
+  }else {
+    var bestMoveValue = -100;
+    var move = 0;
+    for(var i = 0; i < bord.length; i++){
+      var newBoard = this.checkMove(i, this.computer, board);
+      if(newBoard){
+        var predictedMoveValue = this.maxValue(newBoard);
+        if(predictedMoveValue > bestMoveValue) {
+          bestMoveValue = predictedMoveValue;
+          move = i;
+        }
+      }
+    }
+    return bestMoveValue;
+  }
+};
